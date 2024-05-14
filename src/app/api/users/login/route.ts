ad666b64dd06bf07607d8c22ca774b7795f0ae1f
AbhishekTyagi0 <abhishektyagigi4310@gmail.com>
2024-05-14 21:44:25 +0530

    added backend and api

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,54 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    console.log("Login reqBody", reqBody);
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    console.log("user Exits");
+
+    const validPass = await bcryptjs.compare(password, user.password);
+
+    if (!validPass) {
+      return NextResponse.json(
+        { error: "checkout your credentials" },
+        { status: 400 }
+      );
+    }
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login successful",
+      success: true,
+    });
+
+    response.cookies.set("token", token, { httpOnly: true });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
